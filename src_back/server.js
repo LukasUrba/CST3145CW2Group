b819dd42cf2047d9fd0a4aa578e2de2c12d62be0
Edoardo Fratantonio <ef364@mdx.live.ac.uk>
2022-01-14 16:55:40 +0000

    DIsplay lessons

--- src_back/server.js
+++ src_back/server.js
@@ -1,42 +1,41 @@
 var express = require("express");
 var path = require("path");
-const cors = require('cors');
+
+var fs = require('fs');
 
 var app = express();
-
-var publicPath = path.resolve(__dirname, "public");
+var publicPath = path.join(__dirname, "../public/");
 app.use(express.static(publicPath));
 
-app.use(cors());
-
-app.use(function(request,response,next) {
+app.use(function (request, response, next) {
     console.log("Request IP: " + request.url);
     console.log("Request date: " + new Date());
     next();
 });
 
-
-// console.log(obj);
-app.get("/lessons", function(request,response) {
-    response.end( `
-            {"topic": "MATH", "location": "London", "price": 100 },
-            { "topic": "Science", "location": "Liverpool", "price": 80 },
-            { "topic": "History", "location": "Oxford", "price": 90 },
-            { "topic": "Geography", "location": "Bristol", "price": 120} `
+app.get("/lessons", function (request, response) {
+    response.end(`
+            {   "topic": "MATH", "location": "London", "price": 100         },
+            {   "topic": "Science", "location": "Liverpool", "price": 80    },
+            {   "topic": "History", "location": "Oxford", "price": 90       },
+            {   "topic": "Geography", "location": "Bristol", "price": 120   } `
     );
-    // var fs = require('fs');
-    // var obj = JSON.parse(fs.createReadStream('../index.html', 'utf8'));
-    // console.log(obj);
 
 });
+
 // comment
-app.get("/user", function(request, response) {
+app.get("/user", function (request, response) {
     // response.render
     response.end("{'email': 'user@email.com', 'password': 'mypassword'}");
 });
 
-app.use(function(request, response) {
+app.use(function (request, response) {
     response.status(404).send("This page has not been made yet!");
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
+
+
+
