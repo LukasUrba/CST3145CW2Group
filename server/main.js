593d6d103ba71dc1eb2a99d1bcc2f60db39c6cca
Francy <fa1033@live.mdx.ac.uk>
2022-01-16 17:09:09 +0000

    code cleaned and further comments added

--- server/main.js
+++ server/main.js
@@ -1,47 +1,36 @@
-const express   = require("express");   // web framework
-const path      = require("path");      // resources path handler
-const cors      = require("cors")       // allow cross origin access
-const http      = require("http");      // 
 
-const urlModule = require("url");       // url sections handler
-const router = express.Router();        // 
-const statusCodes = require("../json/statusCodes.json");    // status codes collection
+const express		= require("express");	// web framework
+const path			= require("path");		// resources path handler
+const cors			= require("cors")		// allow cross origin access
 
-const app = express();                  // middlewares handler
+const urlModule		= require("url");		// url sections handler
+const statusCodes	= require("../json/statusCodes.json");	// status codes collection
+const publicPath	= path.resolve(__dirname, "../public");	// public/static folder path
 
-// Sets up the path where your static files are
-const publicPath = path.resolve(__dirname, "../public");    // public/static folder path
-// Sends static files from the publicPath directory
-app.use(express.static(publicPath));
+const app			= express();			// middlewares handler
 
-app.use(cors());
+app.use(express.static(publicPath));		// Sends static files from the publicPath directory
+app.use(cors());							// allowing cross origins access
+
 
 // requestes monitor
 app.use(function(request,response,next) {
     console.log("Request IP: " + request.ip);
 	console.log("Request URL: " + request.url);
     console.log("Request date: " + new Date());
-    next();
+    next(); // computing the next middleware here below
 });
 
-// var fs = require('fs');
-// var obj = JSON.parse(fs.readFileSync('public/lessons.json', 'utf8'));
 
-
-/* app.use(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Looks like you did not find a static file.");
-});
- */
 // lessons page
-app.get("/lessons", function(request, response) {
+app.get("lessons", function(request, response) {
     if      (!request.query.ajax)               response.sendFile(path.join(__dirname+'/../client/lessons.html'));
     else if (request.query.ajax === "lessons")	response.sendFile(path.join(__dirname+'/../json/lessons.json'));
     else                                        response.status(statusCodes.clientError.badRequest).end();
 });
     
 // User page
-app.get("/user", function(request, response) {
+app.get("user", function(request, response) {
     // response.render
     if		(!request.query.ajax)			response.sendFile(path.join(__dirname+'/../client/user.html'));
     else if (request.query.ajax === "user")	response.sendFile(path.join(__dirname+'/../json/user.json'));
@@ -54,8 +43,6 @@ app.use(function(request, response) {
     response.sendFile(path.join(__dirname+'/../client/404.html'));
 });
 
-//add the router
-app.use('/', router);
 
 // Starts the app on port 3000 and display a message when it’s started
 app.listen(3000, console.log("App started on port 3000"));
